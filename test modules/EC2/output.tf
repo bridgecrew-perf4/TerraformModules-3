54dd9825e378e1616ca30746f06cee2516d9129b
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-02-12 15:07:44 +0100

    Update output.tf

--- test modules/EC2/output.tf
+++ test modules/EC2/output.tf
@@ -1,3 +1,4 @@
 output "primary_network_interface_id" {
-  value = aws_instance.ec2.*.primary_network_interface_id
+  description = "List of IDs of the primary network interface of instances"
+  value       = aws_instance.ec2.*.primary_network_interface_id
 }
