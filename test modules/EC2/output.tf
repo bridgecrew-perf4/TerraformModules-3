9a848da744d7f1292085a9f7290705bc3fedc465
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-02-12 14:31:23 +0100

    Update output.tf

--- test modules/EC2/output.tf
+++ test modules/EC2/output.tf
@@ -1,7 +1,7 @@
 output "primary_network_interface_id" {
-  value = aws_instance.test-ec2deployment.primary_network_interface_id
+  value = aws_instance.ec2.primary_network_interface_id
 }
 
 output "public_ip" {
-  value = aws_instance.test-ec2deployment.public_ip
+  value = aws_instance.ec2.public_ip
 }
