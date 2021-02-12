e5d2afdf4ee1eeab2e528683b6b097c8775d089e
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-02-12 15:37:14 +0100

    Update output.tf

--- test modules/EC2/output.tf
+++ test modules/EC2/output.tf
@@ -2,3 +2,8 @@ output "primary_network_interface_id" {
   description = "List of IDs of the primary network interface of instances"
   value       = aws_instance.ec2.*.primary_network_interface_id
 }
+
+output "vpc_security_group_ids" {
+  description = "List of associated security groups of instances, if running in non-default VPC"
+  value       = aws_instance.*.vpc_security_group_ids
+}
