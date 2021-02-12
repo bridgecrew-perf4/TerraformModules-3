050f46c3d4507fcdefb80eae9575358976bffaa3
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-02-12 14:13:46 +0100

    Update variables.tf

--- test modules/EC2/variables.tf
+++ test modules/EC2/variables.tf
@@ -44,8 +44,31 @@ variable "server_port" {
 # ---------------------------------------------------------------------------------------------------------------------
 # Server specific settings
 # ---------------------------------------------------------------------------------------------------------------------
+variable "instance_count" {
+  description = "Number of instances to launch"
+  type        = number
+  default     = 1
+}
 
-variable "env_name" {
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized"
+  type        = bool
+  default     = false
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
+  type        = string
+  default     = ""
+}
+
+variable "user_data" {
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
+  type        = string
+  default     = null
+}
+
+variable "name" {
   description = "Name of your server, this value will be used for the server name tag and the tag for the security group name."
   type        = string
   default     = "Default_terraform_deployment"
@@ -55,12 +78,20 @@ variable "volume_size" {
   default = "60"
 }
 
-variable "schedule" {
-  default = "TESTING"
-}
-
 variable "instance_type" {
   default = "t3a.large"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "ebs_block_device" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(map(string))
+  default     = []
 }
 
 #Packer creates two images, a 2016 and 2019 one. In order to always have to most up-to date one, the following options are available. You need to choose one in the next variable.
