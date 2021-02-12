6dc0e68d9dd2addcc1434fb1787802d1db8f4c9b
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-02-12 15:30:22 +0100

    Update main.tf

--- test modules/EC2/main.tf
+++ test modules/EC2/main.tf
@@ -9,11 +9,11 @@ data "aws_ami" "latest" {
 }
 
 resource "aws_instance" "ec2" {
-  count                       = var.instance_count
-  ami                         = data.aws_ami.latest.id
-  instance_type               = var.instance_type
-  user_data                   = var.user_data
-  key_name                    = var.key_name
+  count         = var.instance_count
+  ami           = data.aws_ami.latest.id
+  instance_type = var.instance_type
+  user_data     = var.user_data
+  key_name      = var.key_name
   subnet_id = length(var.network_interface) > 0 ? null : element(
     distinct(compact(concat([var.subnet_id], var.subnet_ids))),
     count.index,
@@ -21,14 +21,22 @@ resource "aws_instance" "ec2" {
   availability_zone           = var.availability_zone
   associate_public_ip_address = var.public_ip
 
-  ebs_optimized               = var.ebs_optimized
+  ebs_optimized = var.ebs_optimized
 
   root_block_device {
     volume_type           = "gp2"
     volume_size           = var.volume_size
     delete_on_termination = false
   }
-    dynamic "ebs_block_device" {
+    dynamic "network_interface" {
+    for_each = var.network_interface
+    content {
+      device_index          = network_interface.value.device_index
+      network_interface_id  = lookup(network_interface.value, "network_interface_id", null)
+      delete_on_termination = lookup(network_interface.value, "delete_on_termination", false)
+    }
+  }
+  dynamic "ebs_block_device" {
     for_each = var.ebs_block_device
     content {
       delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
@@ -40,14 +48,5 @@ resource "aws_instance" "ec2" {
       volume_size           = lookup(ebs_block_device.value, "volume_size", null)
       volume_type           = lookup(ebs_block_device.value, "volume_type", null)
     }
-        dynamic "network_interface" {
-    for_each = var.network_interface
-    content {
-      device_index          = network_interface.value.device_index
-      network_interface_id  = lookup(network_interface.value, "network_interface_id", null)
-      delete_on_termination = lookup(network_interface.value, "delete_on_termination", false)
-    }
-          }
-      
   }
 }
