3e5177d9be695b5ae2ddf62061c1709bb6b155d4
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-03-02 16:16:48 +0100

    Update variables.tf

--- test modules/EC2/variables.tf
+++ test modules/EC2/variables.tf
@@ -147,6 +147,7 @@ variable "default_ami" {
     "centos7" = "CentOS7-CUSTOM*"
     "centos8" = "CentOS8-CUSTOM*"
     "rhel"    = "RHEL8-CUSTOM*"
+    "nessus"  = "Nessus 8.13.1 (master-12 1608153355) BYOL-8e783acf-0dfb-44dc-b080-415aad141bb2-ami-04aeca910673b8c21.4"
   }
 }
 
