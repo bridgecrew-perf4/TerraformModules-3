9807be80e193191cab6dece962017a527f1216c9
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-05-04 12:23:46 +0200

    Added ecs without LB

---
+++ test modules/ECS service - Without LB/variables.tf
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------
+# ECS Service
+#------------------------------------------------------------------------------
+variable "name" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+}
+
+variable "cluster" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+}
+
+variable "task_definition" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+}
+
+variable "desired_count" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  default     = "1"
+}
+
+variable "launch_type" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  default     = "FARGATE"
+}
+
+variable "security_groups" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+}
+
+variable "subnets" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+}
+
+variable "assign_public_ip" {
+  description = "The name of the cluster (up to 255 letters, numbers, hyphens, and underscores)"
+  default     = "true"
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
