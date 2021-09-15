f6922d3bb03f297953096a4924d9ef4b56bb30a6
rckvwijk <76739720+rckvwijk@users.noreply.github.com>
2021-09-15 16:28:44 +0200

    Update output.tf

--- test modules/ECS service/output.tf
+++ test modules/ECS service/output.tf
@@ -10,3 +10,8 @@ output "aws_ecs_service_cluster_name" {
   description = "The name of the ECS service"
   value       = aws_ecs_service.main.cluster
 }
+
+output "aws_ecs_service_cluster_id" {
+  description = "ARN of the ECS service."
+  value       = aws_ecs_service.main.id
+}
